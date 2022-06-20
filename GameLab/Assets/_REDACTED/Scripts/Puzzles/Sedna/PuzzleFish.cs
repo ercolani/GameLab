c02e5f0893bf930fac7a4789e8d0a410011b6294
ercolani <games.erco@outlook.com>
2022-06-20 15:19:40 +0200

    Updated sedna puzzle
    Updated sedna puzzle and ekeko

--- GameLab/Assets/_REDACTED/Scripts/Puzzles/Sedna/PuzzleFish.cs
+++ GameLab/Assets/_REDACTED/Scripts/Puzzles/Sedna/PuzzleFish.cs
@@ -24,14 +24,28 @@ public class PuzzleFish : MonoBehaviour
     private float _rotationSpeed = 0.1f;
 
     /// <summary>
-    /// The current
+    /// Wether or not the fish should follow the waypoint system.
     /// </summary>
-    private int _targetIndex = 1;
+    public bool FollowWaypoints
+    {
+        get => _followWaypoints;
+        set => _followWaypoints = value;
+    }
 
     /// <summary>
     /// The current
     /// </summary>
     public int TargetIndex => _targetIndex;
+
+    /// <summary>
+    /// The current torch that the fish follows.
+    /// </summary>
+    private int _currentTorchIndex;
+
+    /// <summary>
+    /// The current
+    /// </summary>
+    private int _targetIndex = 1;
 
     /// <summary>
     /// Wether or not the fish should follow the waypoint system.
@@ -41,20 +55,16 @@ public class PuzzleFish : MonoBehaviour
     /// <summary>
     /// Wether or not the fish should follow the waypoint system.
     /// </summary>
-    public bool FollowWaypoints 
-    {
-        get => _followWaypoints;
-        set => _followWaypoints = value;
-    }
-
-    /// <summary>
-    /// Wether or not the fish should follow the waypoint system.
-    /// </summary>
     public float MovementSpeed
     {
         get => _movementSpeed;
         set => _movementSpeed = value;
     }
+
+    /// <summary>
+    /// The current torch that the fish follows.
+    /// </summary>
+    public int CurrentTorchIndex => _currentTorchIndex;
 
     private void Update()
     {
@@ -91,9 +101,10 @@ public class PuzzleFish : MonoBehaviour
     /// </summary>
     /// <param name="_waypoints">The new set of waypoints</param>
     /// <param name="startWaypoint">The starting index</param>
-    public void UpdateWaypoints(Transform[] _waypoints)
+    public void UpdateWaypoints(Transform[] _waypoints, int torchIndex)
     {
         _currentWaypoints = _waypoints;
         _targetIndex = 0;
+        _currentTorchIndex = torchIndex;
     }
 }
